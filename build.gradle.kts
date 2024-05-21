8ff4cd0b86339d4dc88c6a4f191e3f1100806a9e
Jan Skrasek <hrach.cz@gmail.com>
2024-05-21 23:43:19 +0200

    update to AGP 8.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 	id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 	id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.14.0" apply false
 	id("org.jmailen.kotlinter") version "4.3.0" apply false
-	id("com.android.application") version "8.4.0" apply false
+	id("com.android.application") version "8.4.1" apply false
 	id("com.vanniktech.maven.publish") version "0.28.0" apply false
 	id("com.gradleup.nmcp") version "0.0.7"
 }
