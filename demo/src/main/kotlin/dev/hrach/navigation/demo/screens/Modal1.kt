8983d6d42e957df7dc46678f72fd5f3d9e787ab7
Jan Skrasek <hrach.cz@gmail.com>
2024-10-04 20:58:24 +0200

    modalsheet: fix handling custom BackHandler in modalsheet

--- demo/src/main/kotlin/dev/hrach/navigation/demo/screens/Modal1.kt
+++ demo/src/main/kotlin/dev/hrach/navigation/demo/screens/Modal1.kt
@@ -1,22 +1,30 @@
 package dev.hrach.navigation.demo.screens
 
 import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import dev.hrach.navigation.demo.Destinations
 import dev.hrach.navigation.results.NavigationResultEffect
@@ -42,6 +50,11 @@ private fun Modal1(
 	navigate: (Any) -> Unit,
 	bottomSheetResult: Int,
 ) {
+	var disableBackHandling by rememberSaveable { mutableStateOf(false) }
+	BackHandler(disableBackHandling) {
+		// no-op
+	}
+
 	Surface(
 		color = MaterialTheme.colorScheme.inverseSurface,
 	) {
@@ -58,6 +71,13 @@ private fun Modal1(
 				Text("BottomSheet")
 			}
 			Text("BottomSheetResult: $bottomSheetResult")
+
+			Spacer(Modifier.height(32.dp))
+
+			Row(verticalAlignment = Alignment.CenterVertically) {
+				Text("Disable back handling")
+				Switch(disableBackHandling, onCheckedChange = { disableBackHandling = it })
+			}
 		}
 	}
 }
